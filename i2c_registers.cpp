f7cf51505add76572e72858135ec686f6d4efc72
MartinD-CZ <danekmartin45@gmail.com>
2026-03-19 22:20:37 +0100

    reworked to single-byte registers

--- i2c_registers.cpp
+++ i2c_registers.cpp
@@ -7,94 +7,82 @@
 #include <cstring>
 
 
-void I2cSlaveRegisters::init(uint8_t myAddress, size_t numRegisters, uint_fast8_t irqPriority)
+void I2cSlaveRegisters::init(uint8_t myAddress, uint8_t numRegisters, uint_fast8_t irqPriority)
 {
 	m_i2cSlave.init(myAddress, Callback{&ClassCallbackHelper<I2cSlaveRegisters, &I2cSlaveRegisters::addrMatchCallback>, this}, irqPriority);
 	m_i2cSlave.setCallback(I2c::CallbackType::RX, Callback{&ClassCallbackHelper<I2cSlaveRegisters, &I2cSlaveRegisters::rxCallback>, this});
-	m_i2cSlave.setCallback(I2c::CallbackType::STOP, Callback{&ClassCallbackHelper<I2cSlaveRegisters, &I2cSlaveRegisters::stopCallback>, this});
 	m_i2cSlave.setCallback(I2c::CallbackType::TXIS, Callback{&ClassCallbackHelper<I2cSlaveRegisters, &I2cSlaveRegisters::txisCallback>, this});
-	m_registers = (RegEntry*)mal::salloc(numRegisters * sizeof(RegEntry));
-	m_numRegisters = 0;
-	m_maxRegisters = numRegisters;
+	m_i2cSlave.setCallback(I2c::CallbackType::STOP, Callback{&ClassCallbackHelper<I2cSlaveRegisters, &I2cSlaveRegisters::stopCallback>, this});
+
+	m_registers = (uint8_t*)mal::salloc(numRegisters);
+	memset(m_registers, 0x00, numRegisters);
+
+	m_numRegisters = numRegisters;
+	m_lastRxTxPos = 0;
 }
 
 
-void I2cSlaveRegisters::addRegister(uint8_t address, size_t sizeBytes)
+size_t I2cSlaveRegisters::getNumRegisters() const
 {
-	ASSERT(m_numRegisters < m_maxRegisters);
-	m_registers[m_numRegisters].regAddress = address;
-	m_registers[m_numRegisters].dataPtr = (uint8_t*)mal::salloc(sizeBytes);
-	m_registers[m_numRegisters].sizeBytes = sizeBytes;
-	m_numRegisters++;
+	return m_numRegisters;
 }
 
 
-bool I2cSlaveRegisters::setRegister(uint8_t address, const void* data, size_t numbytes)
+bool I2cSlaveRegisters::setRegisters(uint8_t startAddress, const void* data, size_t numBytes)
 {
-	auto const reg = getRegisterPtr(address);
-	if (reg == nullptr || numbytes > reg->sizeBytes)
+	if ((startAddress + numBytes) > m_numRegisters)
 		return false;
 
-	memcpy(reg->dataPtr, data, numbytes);
+	memcpy(&m_registers[startAddress], data, numBytes);
 	return true;
 }
 
 
-size_t I2cSlaveRegisters::getRegister(uint8_t address, void* data, size_t maxBytes) const
+uint8_t I2cSlaveRegisters::getRegister(uint8_t address) const
 {
-	auto const reg = getRegisterPtr(address);
-	if (reg == nullptr || maxBytes < reg->sizeBytes)
+	if (address >= m_numRegisters)
 		return 0;
 
-	memcpy(data, reg->dataPtr, reg->sizeBytes);
-	return reg->sizeBytes;
+	return m_registers[address];
+}
+
+
+void I2cSlaveRegisters::logRegisters() const
+{
+	LOGI("[I2C REGS] Reg\tVal\n");
+	for (uint8_t i = 0; i < m_numRegisters; i++)
+		LOGI_NOINTRO("\t\t\t\t\t%02X\t%02X\n", i, m_registers[i]);
 }
 
 
 void I2cSlaveRegisters::addrMatchCallback()
 {
 	m_lastRxTxPos = 0;
+	m_i2cSlave.flushTxRegister();
 }
 
 
 void I2cSlaveRegisters::rxCallback()
 {
-	if (m_lastRxTxPos < sizeof(m_lastRx))
+	const auto value = m_i2cSlave.read();
+	
+	if (m_lastRxTxPos == 0)
+		m_currentAddress = value;
+	else
 	{
-		m_lastRx[m_lastRxTxPos] = m_i2cSlave.read();
-		m_lastRxTxPos = m_lastRxTxPos + 1;
+		const auto index = m_currentAddress + m_lastRxTxPos - 1;
+		if (index < m_numRegisters)
+			m_registers[index] = value;	
 	}	
-}
 
-
-void I2cSlaveRegisters::stopCallback()
-{
-	if (!m_i2cSlave.isReadRequest() && m_lastRxTxPos > 1)		//if this is a write request and at least 2 bytes were received (register address + at least 1 byte of data)
-	{
-		const auto reg = getRegisterPtr(m_lastRx[0]);
-		if ((reg != nullptr) && (reg->sizeBytes == m_lastRxTxPos - 1))		//if the register exists and the number of received data bytes matches the register size
-			for (size_t i = 0; i < reg->sizeBytes; i++)			//we can't use memcpy here
-				reg->dataPtr[i] = m_lastRx[i + 1];
-	}
+	m_lastRxTxPos = m_lastRxTxPos + 1;
 }
 
 
 void I2cSlaveRegisters::txisCallback()
 {
-	const auto reg = getRegisterPtr(m_lastRx[0]);
-	uint8_t value = 0;
-	if (reg != nullptr && reg->sizeBytes > 0 && m_lastRxTxPos < reg->sizeBytes)
-		value = reg->dataPtr[reg->sizeBytes - 1 - m_lastRxTxPos];		//we need to send in big-endian order
+	const auto index = m_currentAddress + m_lastRxTxPos;
+	const auto value = (index < m_numRegisters) ? m_registers[index] : 0;
 	m_i2cSlave.write(value);
 	m_lastRxTxPos = m_lastRxTxPos + 1;
-}
-
-
-I2cSlaveRegisters::RegEntry* I2cSlaveRegisters::getRegisterPtr(uint8_t address) const
-{
-	for (size_t i = 0; i < m_numRegisters; i++)
-		if (m_registers[i].regAddress == address)
-			return &m_registers[i];
-		
-	return nullptr;
 }
--- i2c_registers.h
+++ i2c_registers.h
@@ -12,41 +12,34 @@ class I2cSlaveRegisters
 public:
 	I2cSlaveRegisters(I2cSlave& i2cSlave): m_i2cSlave(i2cSlave) {};
 
-	void init(uint8_t myAddress, size_t numRegisters, uint_fast8_t irqPriority = LOWEST_IRQ_PRIORITY);
+	void init(uint8_t myAddress, uint8_t numRegisters, uint_fast8_t irqPriority = LOWEST_IRQ_PRIORITY);
 
-	void addRegister(uint8_t address, size_t sizeBytes);
 	size_t getNumRegisters() const;
-	bool setRegister(uint8_t address, const void* data, size_t numbytes);
+	bool setRegisters(uint8_t startAddress, const void* data, size_t numBytes);
 
 	template <typename T>
 	bool setRegister(uint8_t address, T value)
 	{
-		return setRegister(address, &value, sizeof(value));
+		return setRegisters(address, &value, sizeof(value));
 	}
 
-	size_t getRegister(uint8_t address, void* data, size_t maxBytes) const;
+	uint8_t getRegister(uint8_t address) const;
+
+	void logRegisters() const;
 
 private:
-	struct RegEntry
-	{
-		uint8_t regAddress;
-		uint8_t* dataPtr;
-		size_t sizeBytes;
-	};	
-
 	void addrMatchCallback();
 	void rxCallback();
-	void stopCallback();
 	void txisCallback();
-
-	RegEntry* getRegisterPtr(uint8_t address) const;
+	void stopCallback() {
+		//m_lastRxTxPos = 0;
+	};
 
 	I2cSlave& m_i2cSlave;
 	
-	RegEntry* m_registers;
-	size_t m_numRegisters;
-	size_t m_maxRegisters;
+	uint8_t* m_registers;
+	uint8_t m_numRegisters;
 
-	volatile uint8_t m_lastRx[5];
-	volatile size_t m_lastRxTxPos;
+	volatile uint8_t m_currentAddress = 0;
+	volatile uint8_t m_lastRxTxPos = 0;
 };
